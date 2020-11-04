ea67e045bc89fb490cc1e2984290432ad3d9ddbd
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2020-11-04 10:33:18 +0100

    primera version

---
+++ html/wp-content/themes/MiPrimerTema/footer.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1> FOOTER </h1>";
+?>
+<img src="<?php echo get_stylesheet_directory_uri();?>/images/abc.png" >
---
+++ html/wp-content/themes/MiPrimerTema/header.php
@@ -0,0 +1,13 @@
+<div id="header">
+ <div id="headerimg">
+   <h1>
+    <a href="<?php echo get_option('home'); ?>">
+       <?php bloginfo('name'); ?></a>
+</h1>
+<div class="description">
+	<?php bloginfo('description'); ?>
+	<br>
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/header.jpeg" >
+</div>
+</div>
+</div>
